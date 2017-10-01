60473055ad5ae77ecd7a48877bc1366abc4bc1ba
Fernando <fernandoza@gmail.com>
2017-10-01 18:17:00 -0500

    Arrays equal.

--- src/test/java/SampleAssertions.java
+++ src/test/java/SampleAssertions.java
@@ -1,4 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class SampleAssertions {
 
+    public SampleAssertions(){
+
+    }
+
+    @Test
+    public void testAssertArrayEquals(){
+        byte[] expected = "test".getBytes();
+        byte[] actual = "test".getBytes();
+        assertArrayEquals("failure - byte arrays are not the same", expected, actual);
+    }
 
 }
