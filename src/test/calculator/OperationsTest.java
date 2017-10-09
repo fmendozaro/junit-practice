5e9ebe60f1d01c426c56b7ea6f144d7dbbed22f0
Fernando <fernandoza@gmail.com>
2017-10-09 14:33:58 -0500

    Create the tests for the calc class. Finished all of them.

--- src/test/calculator/OperationsTest.java
+++ src/test/calculator/OperationsTest.java
@@ -6,21 +6,52 @@ import static org.junit.Assert.*;
 
 public class OperationsTest {
 
+    Calc calc = new Calc();
+    // delta is to determine the floating numbers precision
+    double delta = 0.1, expectedResult = 0, res = 0;
+
     public OperationsTest() {}
 
     @BeforeClass
-    public static void setUpClass() throws Exception {}
+    public static void setUp() throws Exception {}
 
     @AfterClass
-    public static void tearDownClass() throws Exception {}
+    public static void tearDown() throws Exception {}
 
     @Test
-    public void testSum() throws Exception {
+    public void sum() {
         double[] nums = {1,2,3,4,5,6};
-        double expectedRes = 21;
-        Calc calc = new Calc();
-        double res = calc.sum(nums);
-        assertEquals(res, expectedRes, 0.1);
+        expectedResult = 21;
+        res = calc.sum(nums);
+        assertEquals(res, expectedResult, delta);
+    }
+
+    @Test
+    public void substract() {
+        res = calc.subtract(17,5);
+        expectedResult = 12;
+        assertEquals(res, expectedResult, delta);
+    }
+
+    @Test
+    public void multiply(){
+        double a = 10, b = 34;
+        expectedResult = 340;
+        res = calc.multiply(a, b);
+        assertEquals(res, expectedResult, delta);
+    }
+
+    @Test
+    public void divide(){
+        double numerator = 0, denominator = 0;
+        expectedResult = 0;
+        res = calc.divide(numerator, denominator);
+        assertEquals(res, expectedResult, delta);
+        numerator = 10;
+        denominator = 2;
+        expectedResult = 5;
+        res = calc.divide(numerator, denominator);
+        assertEquals(res, expectedResult, delta);
     }
 
 }
